b5b3864ab7a8d998b0942c46350919777c758a95
LocalTrickster <eduardoacostacrick58@hotmail.com>
2025-06-17 18:15:52 -0300

    first step

--- main.js
+++ main.js
@@ -1,34 +1,47 @@
 import HelloWorldScene from "./scenes/HelloWorldScene.js";
 
+// global game options
+let gameOptions = {
+    platformStartSpeed: 350,
+    spawnRange: [100, 350],
+    platformSizeRange: [50, 250],
+    playerGravity: 900,
+    jumpForce: 400,
+    playerStartPosition: 200,
+    jumps: 2
+};
+
+let game;
+
+// playGame scene
+class playGame extends Phaser.Scene{
+    constructor(){
+        super("PlayGame");
+    }
+}
+
 // Create a new Phaser config object
 const config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  scale: {
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
-    min: {
-      width: 800,
-      height: 600,
-    },
-    max: {
-      width: 1600,
-      height: 1200,
-    },
-  },
-  physics: {
-    default: "arcade",
-    arcade: {
-      gravity: { y: 200 },
-      debug: true,
-    },
-  },
-  // List of scenes to load
-  // Only the first scene will be shown
-  // Remember to import the scene before adding it to the list
-  scene: [HelloWorldScene],
+    type: Phaser.AUTO,
+    width: 1334,
+    height: 750,
+    scene: [HelloWorldScene],
+    backgroundColor: 0x444444,
+
+    // physics settings
+    physics: {
+        default: "arcade",
+        arcade: {
+          debug: true,
+          
+        }
+    }
 };
 
 // Create a new Phaser game instance
-window.game = new Phaser.Game(config);
+window.onload = function() {
+    game = new Phaser.Game(config);
+    window.focus();
+    resize();
+    window.addEventListener("resize", resize, false);
+};
--- scenes/HelloWorldScene.js
+++ scenes/HelloWorldScene.js
@@ -1,46 +1,123 @@
-// URL to explain PHASER scene: https://rexrainbow.github.io/phaser3-rex-notes/docs/site/scene/
+// URL to explain PHASER scene: https://rexrainbow.github.io/phaser3-rex-notes/docs/site/
+
+// Opciones del juego
+const gameOptions = {
+    platformStartSpeed: 350,
+    spawnRange: [100, 350],
+    platformSizeRange: [50, 250],
+    playerGravity: 900,
+    jumpForce: 400,
+    playerStartPosition: 200,
+    jumps: 2
+};
 
 export default class HelloWorldScene extends Phaser.Scene {
-  constructor() {
-    // key of the scene
-    // the key will be used to start the scene by other scenes
-    super("hello-world");
-  }
+    constructor() {
+        super("hello-world");
+    }
 
-  init() {
-    // this is called before the scene is created
-    // init variables
-    // take data passed from other scenes
-    // data object param {}
-  }
+    preload() {
+        this.load.image("platform", "./public/assets/platform.png");
+        this.load.image("player", "./public/assets/Ninja.png");
+    }
 
-  preload() {
-    // load assets
-    this.load.image("sky", "./assets/space3.png");
-    this.load.image("logo", "./assets/phaser3-logo.png");
-    this.load.image("red", "./assets/particles/red.png");
-  }
+    create() {
+        const config = this.sys.game.config;
 
-  create() {
-    // create game objects
-    this.add.image(400, 300, "sky");
+        // Grupos de plataformas
+        this.platformGroup = this.add.group({
+            removeCallback: function(platform){
+                platform.scene.platformPool.add(platform)
+            }
+        });
 
-    const logo = this.physics.add.image(400, 100, "logo");
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
+        this.platformPool = this.add.group({
+            removeCallback: function(platform){
+                platform.scene.platformGroup.add(platform)
+            }
+        });
 
-    // emmit particles from logo
-    const emitter = this.add.particles(0, 0, "red", {
-      speed: 100,
-      scale: { start: 1, end: 0 },
-      blendMode: "ADD",
-    });
+        this.playerJumps = 0;
 
-    emitter.startFollow(logo);
-  }
+        // Plataforma inicial
+        this.addPlatform(config.width, config.width / 2);
 
-  update() {
-    // update game objects
-  }
+        // Jugador
+        this.player = this.physics.add.sprite(gameOptions.playerStartPosition, config.height / 2, "player");
+        this.player.setGravityY(gameOptions.playerGravity);
+        this.player.setScale(0.25); // Escala más pequeña
+
+        this.physics.add.collider(this.player, this.platformGroup);
+
+        this.input.on("pointerdown", this.jump, this);
+    }
+
+    addPlatform(platformWidth, posX){
+        const config = this.sys.game.config;
+        let platform;
+        if(this.platformPool.getLength()){
+            platform = this.platformPool.getFirst();
+            platform.x = posX;
+            platform.active = true;
+            platform.visible = true;
+            this.platformPool.remove(platform);
+        }
+        else{
+            platform = this.physics.add.sprite(posX, config.height * 0.8, "platform");
+            platform.setImmovable(true);
+            platform.setVelocityX(gameOptions.platformStartSpeed * -1);
+            this.platformGroup.add(platform);
+        }
+        platform.displayWidth = platformWidth;
+        this.nextPlatformDistance = Phaser.Math.Between(gameOptions.spawnRange[0], gameOptions.spawnRange[1]);
+    }
+
+    jump(){
+        if(this.player.body.touching.down || (this.playerJumps > 0 && this.playerJumps < gameOptions.jumps)){
+            if(this.player.body.touching.down){
+                this.playerJumps = 0;
+            }
+            this.player.setVelocityY(gameOptions.jumpForce * -1);
+            this.playerJumps ++;
+        }
+    }
+
+    update(){
+        const config = this.sys.game.config;
+        if(this.player.y > config.height){
+            this.scene.restart();
+        }
+        this.player.x = gameOptions.playerStartPosition;
+
+        let minDistance = config.width;
+        this.platformGroup.getChildren().forEach(function(platform){
+            let platformDistance = config.width - platform.x - platform.displayWidth / 2;
+            minDistance = Math.min(minDistance, platformDistance);
+            if(platform.x < - platform.displayWidth / 2){
+                this.platformGroup.killAndHide(platform);
+                this.platformGroup.remove(platform);
+            }
+        }, this);
+
+        if(minDistance > this.nextPlatformDistance){
+            var nextPlatformWidth = Phaser.Math.Between(gameOptions.platformSizeRange[0], gameOptions.platformSizeRange[1]);
+            this.addPlatform(nextPlatformWidth, config.width + nextPlatformWidth / 2);
+        }
+    }
+}
+
+function resize(){
+    let canvas = document.querySelector("canvas");
+    let windowWidth = window.innerWidth;
+    let windowHeight = window.innerHeight;
+    let windowRatio = windowWidth / windowHeight;
+    let gameRatio = game.config.width / game.config.height;
+    if(windowRatio < gameRatio){
+        canvas.style.width = windowWidth + "px";
+        canvas.style.height = (windowWidth / gameRatio) + "px";
+    }
+    else{
+        canvas.style.width = (windowHeight * gameRatio) + "px";
+        canvas.style.height = windowHeight + "px";
+    }
 }
